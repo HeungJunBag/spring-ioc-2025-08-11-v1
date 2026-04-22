e646296aaa3261cec140288b4a041e35685fd386
HeungJunBag <p2005hihi@gmail.com>
2026-04-22 22:44:03 +0900

    feat(t4): testPostRepository Bean 등록 및 null 반환 문제 해결
    - Repository 객체를 beans Map에 추가
    
    - genBean("testPostRepository") 호출 시 정상 반환되도록 수정

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,3 +1,4 @@
+
 package com.ll.framework.ioc;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
@@ -8,34 +9,43 @@ import java.util.Map;
 
 public class ApplicationContext {
 
-    // 빈 저장소: 이름 -> 객체
+    // 빈 저장소: 이름 -> 객체  
     private final Map<String, Object> beans = new HashMap<>();
 
     public ApplicationContext() {
 
-        // 의존성이 없는 것부터 먼저 만든다
+        // 의존성이 없는 것부터 먼저 만든다  
         TestPostRepository testPostRepository = new TestPostRepository();
 
-        // TestPostService 객체를 만들어서 "testPostService"라는 이름으로 등록
-        // 단, testPostRepository를 주입해서 testPostService를 만든다
+        // TestPostService 객체를 만들어서 "testPostService"라는 이름으로 등록  
+        // 단, testPostRepository를 주입해서 testPostService를 만든다  
         TestPostService testPostService = new TestPostService(testPostRepository);
 
-        // Map에 저장
+        // Map에 저장  
+        beans.put("testPostRepository", testPostRepository);
         beans.put("testPostService", testPostService);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T genBean(String beanName) {
-        // Map에서 이름으로 객체를 찾아서 반환
-        // 한 번 저장된 객체는 계속 재사용되므로, t3의 isSameAd(주소값 비교)를 통과!
+        // Map에서 이름으로 객체를 찾아서 반환  
+        // 한 번 저장된 객체는 계속 재사용됨  
         return (T) beans.get(beanName);
     }
 }
 
+
 /*
-* @SuppressWarnings("unchecked")
-*   - 경고를 억제
-*   - "unchecked"
-*       - 억제할 경고의 종류를 지정
-*       - 여기서는, 타입 체크를 하지 않은 형변환에 대한 경고
-* */
+ * @SuppressWarnings("unchecked")
+ *   - 경고를 억제
+ *   - "unchecked"
+ *       - 억제할 경고의 종류를 지정
+ *       - 여기서는, 타입 체크를 하지 않은 형변환에 대한 경고
+ * */
+
+
+/*
+ * t4: testPostRepository 빈에 넣기
+ * testPostRepository 객체를 만들고,
+ * beans에 넣지 않으면 genBean("testPostRepository")가 null을 반환함
+ * */
