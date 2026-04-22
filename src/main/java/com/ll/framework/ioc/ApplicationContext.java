0b8686bb8f3979ff758ad6d4e2f33a258a66520f
HeungJunBag <p2005hihi@gmail.com>
2026-04-22 21:35:21 +0900

    test(t1): ApplicationContext 생성 및 genBean 기본 구조 추가 (null 반환)
    - 예외 없이 생성되도록 기본 생성자 구현
    
    - genBean은 추후 구현 예정으로 null 반환

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -2,10 +2,11 @@ package com.ll.framework.ioc;
 
 public class ApplicationContext {
     public ApplicationContext() {
-
+        // 아직 아무것도 하지 않음
+        // 예외만 안나면 t1은 통과!
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return (T) null; // 일단 null 반환 (컴파일 에러 방지용)
     }
 }
